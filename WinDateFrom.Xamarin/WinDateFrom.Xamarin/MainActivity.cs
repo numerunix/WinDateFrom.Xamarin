07efcd6e92bf47fc51cb2368a06afad9f278fe1e
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-03 02:43:13 +0100

    onlicklistener

--- WinDateFrom.Xamarin/WinDateFrom.Xamarin/MainActivity.cs
+++ WinDateFrom.Xamarin/WinDateFrom.Xamarin/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.Wearable.Activity;
 using Android.Views;
+using Java.Nio.Channels;
 
 /*
   *  This code is distribuited under GPL 3.0 or, at your opinion, any later version
@@ -16,46 +17,17 @@ using Android.Views;
 namespace WinDateFrom.Xamarin
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : WearableActivity, View.IOnClickListener
+    public class MainActivity : WearableActivity
     {
-        EditText date, name;
-        TextView result, anniversary;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.activity_main);
-            date = FindViewById<EditText>(Resource.Id.editText2);
-            name = FindViewById<EditText>(Resource.Id.editText1);
-            result = FindViewById<TextView>(Resource.Id.textView1);
-            anniversary = FindViewById<TextView>(Resource.Id.textView2);
             Button b = FindViewById<Button>(Resource.Id.button1);
-            b.SetOnClickListener(this);
+            b.SetOnClickListener(new MyOnClickListener(FindViewById<EditText>(Resource.Id.editText2), FindViewById<EditText>(Resource.Id.editText1), FindViewById<TextView>(Resource.Id.textView1), FindViewById<TextView>(Resource.Id.textView2), Resources));
             SetAmbientEnabled();
         }
 
-        public void OnClick(View v)
-        {
-            Java.Util.Date d = new Java.Util.Date();
-            Java.Util.Date d1;
-            anniversary.Text = "";
-            result.Text = "";
-            try
-            {
-                d1 = new Java.Util.Date(date.Text);
-            } catch (Java.Lang.IllegalArgumentException ex)
-            {
-                result.Text = Resources.GetString(Resource.String.invalid_rvalue);
-                return;
-            }
-            long diff = d.Time - d1.Time;
-            diff = diff / (1000 * 3600 * 24);
-            result.Text = Resources.GetString(Resource.String.you_meet) + " "+ name.Text+" "+ Resources.GetString(Resource.String.about)+" "+diff.ToString()+" "+ Resources.GetString(Resource.String.days_ago)+".";
-            if (d.GetDate() == d1.GetDate())
-                if (d.Month == d1.Month)
-                    anniversary.Text = Resources.GetString(Resource.String.is_your_anniversary);
-                else
-                    anniversary.Text = Resources.GetString(Resource.String.is_your_mesiversary);
-        }
     }
 }
 
---
+++ WinDateFrom.Xamarin/WinDateFrom.Xamarin/MyOnClickListener.cs
@@ -0,0 +1,46 @@
+﻿using Android.Content.Res;
+using Android.Views;
+using Android.Widget;
+
+namespace WinDateFrom.Xamarin
+{
+    internal class MyOnClickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        EditText date, name;
+        TextView result, anniversary;
+        Resources res;
+
+        public MyOnClickListener(EditText d, EditText n, TextView r, TextView a, Resources re)
+        {
+            date = d;
+            name = n;
+            result = r;
+            anniversary = a;
+            res = re;
+        }
+        public void OnClick(View v)
+        {
+            Java.Util.Date d = new Java.Util.Date();
+            Java.Util.Date d1;
+            anniversary.Text = "";
+            result.Text = "";
+            try
+            {
+                d1 = new Java.Util.Date(date.Text);
+            }
+            catch (Java.Lang.IllegalArgumentException ex)
+            {
+                result.Text = res.GetString(Resource.String.invalid_rvalue);
+                return;
+            }
+            long diff = d.Time - d1.Time;
+            diff = diff / (1000 * 3600 * 24);
+            result.Text = res.GetString(Resource.String.you_meet) + " " + name.Text + " " + res.GetString(Resource.String.about) + " " + diff.ToString() + " " + res.GetString(Resource.String.days_ago) + ".";
+            if (d.GetDate() == d1.GetDate())
+                if (d.Month == d1.Month)
+                    anniversary.Text = res.GetString(Resource.String.is_your_anniversary);
+                else
+                    anniversary.Text = res.GetString(Resource.String.is_your_mesiversary);
+        }
+    }
+}
